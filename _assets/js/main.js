1300e296851fc99694ca6e20b65095ef12126acd
David Carter <dajocarter@gmail.com>
2017-04-09 23:37:00 -0400

    Transition HTML to bootstrap

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,5 +1,19 @@
 jQuery(document).ready(function($) {
   $('input[type="checkbox"], input[type="radio"]').on('change', function() {
-    ( $(this).val() === 'true' ) ? $(this).parents('[class^="checkbox"], [class^="radio"]').toggleClass('correct') : $(this).parents('[class^="checkbox"], [class^="radio"]').toggleClass('false');
+    ( $(this).val() === 'true' ) ? $(this).parents('.checkbox, .radio').toggleClass('correct') : $(this).parents('.checkbox, .radio').toggleClass('false');
+  });
+
+  $('.answer-check').on('input', function() {
+    var input = $(this);
+    var answers = input.data('answers');
+    var userAnswer = input.val();
+    var value = false;
+    for (var i = 0; i < answers.length; i++) {
+      if (answers[i].toLowerCase() === userAnswer.toLowerCase()) {
+        value = true;
+      }
+    }
+    ( value ) ? input.parent('.input-group').addClass('correct').removeClass('false') : input.parent('.input-group').addClass('false').removeClass('correct');
+
   });
 });
